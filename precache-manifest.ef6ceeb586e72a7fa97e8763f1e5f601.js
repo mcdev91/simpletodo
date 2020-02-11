6d74e902b815f0919fc14d4be1d654a6692e59ed
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-02-11 22:29:19 +0200

    Updates

---
+++ precache-manifest.ef6ceeb586e72a7fa97e8763f1e5f601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8628e23b667cc0eb0177c083e142e258",
+    "url": "/simpletodo/index.html"
+  },
+  {
+    "revision": "1713eb681636516f75b9",
+    "url": "/simpletodo/static/css/main.3b2b949f.chunk.css"
+  },
+  {
+    "revision": "12d8f2fc12c1df7577fd",
+    "url": "/simpletodo/static/js/2.7088b401.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simpletodo/static/js/2.7088b401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1713eb681636516f75b9",
+    "url": "/simpletodo/static/js/main.3e4b9ac2.chunk.js"
+  },
+  {
+    "revision": "2352fb1c59cd24e39c74",
+    "url": "/simpletodo/static/js/runtime-main.88742c81.js"
+  }
+]);
